0f54e443d5aa7109becd853a92fb4520f4376e56
YuvalAmshalem <yuval3107@gmail.com>
2021-12-17 11:52:06 +0200

    'python-static-site-generator'

--- ssg.py
+++ ssg.py
@@ -1,8 +1,8 @@
 import typer
-from ssg.site import Site 
+from ssg.site import Site
 
 def main(source="content", dest="dist"):
-    config={"source":source,"dest":dest}
+    config=dict("source"=source,"dest"=dest)
     site=Site(**config)
     site.build()
 
